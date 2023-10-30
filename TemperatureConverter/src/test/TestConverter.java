701ab175c5c60dc6efbea8421678b2120295a23d
CouchPotato <staung1445@gmail.com>
2023-10-30 16:59:20 +0000

    Amend: function call spelling and import of Converter

--- TemperatureConverter/src/test/TestConverter.java
+++ TemperatureConverter/src/test/TestConverter.java
@@ -1,21 +1,21 @@
 package test;
 import org.junit.jupiter.api.Test;
+import utils.Converter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import Converter.Converter;
 
 class TestConverter {
 
     @Test
     void celsiusToFahrenheit() {
         double temperature = 20.0;
-        double conversion = Converter.CelciusToFarenheit(temperature);
+        double conversion = Converter.CelsiusToFahrenheit(temperature);
         assertEquals(68.0, conversion);
     }
 
     @Test
     void fahrenheitToCelsius() {
         double temperature = 68.0;
-        double conversion = Converter.FarenheitToCelcius(temperature);
+        double conversion = Converter.FahrenheitToCelsius(temperature);
         assertEquals(20.0, conversion);
     }
 }
