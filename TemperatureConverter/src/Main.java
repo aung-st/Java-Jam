f72dab10a224faef99b559b64c780137bdbbb1b7
CouchPotato <staung1445@gmail.com>
2023-10-30 15:40:05 +0000

    Amend: function call names

--- TemperatureConverter/src/Main.java
+++ TemperatureConverter/src/Main.java
@@ -2,7 +2,7 @@ import Converter.Converter;
 
 public class Main {
     public static void main(String[] args){
-        System.out.println(Converter.CelciusToFarenheit(20));
-        System.out.println(Converter.FarenheitToCelcius(68));
+        System.out.println(Converter.CelsiusToFahrenheit(20));
+        System.out.println(Converter.FahrenheitToCelsius(68));
     }
 }
