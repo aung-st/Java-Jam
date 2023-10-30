88d7ad3c2c37c9a8b02f1582ea3d2a745805d8d7
CouchPotato <staung1445@gmail.com>
2023-10-30 17:06:07 +0000

    Add: converter class to utils

---
+++ TemperatureConverter/src/utils/Converter.java
@@ -0,0 +1,18 @@
+package utils;
+public class Converter {
+    public static double CelsiusToFahrenheit(double temperature){
+        return (temperature*1.8)+32;
+    }
+    public static double FahrenheitToCelsius(double temperature){
+        return (5/9.0)*(temperature-32);
+    }
+
+    public static double CelsiusToKelvin(double temperature){
+        return temperature + 273.15;
+    }
+
+    public static double KelvinToCelsius(double temperature){
+        return temperature - 273.15;
+    }
+
+}
