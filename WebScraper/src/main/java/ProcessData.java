5aad35b9f2c377afc7e0440c2e1bb320e5fec495
CouchPotato <staung1445@gmail.com>
2023-11-03 15:28:34 +0000

    Amend: formatting of class

--- WebScraper/src/main/java/ProcessData.java
+++ WebScraper/src/main/java/ProcessData.java
@@ -1,39 +1,24 @@
-import org.jsoup.nodes.Document;
-
 import java.io.File;
 import java.io.FileWriter;
 import java.util.List;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 public class ProcessData {
     public static FileWriter makeFile(String name) throws IOException {
-        File file = new File("data/"+name + ".csv");
+        // create a file in the data directory in WebScraper as a csv
+        File file = new File("data/" + name + ".csv");
         try {
             return new FileWriter(file);
-        }catch (IOException e){
+        } catch (IOException e) {
             System.out.println("Something went wrong...");
         }
         return new FileWriter(file);
     }
 
     public static void writeToFile(FileWriter file, List<String> content) throws IOException {
-        try {
-            for (String line : content) {
-                file.write(line + "\n");
-            }
-        } catch (IOException e){
-            System.out.println("Something went wrong...");
+        // write content line by line into selected csv file
+        for (String line : content) {
+            file.write(line + "\n");
         }
-    }
-
-    public static void main(String[] args) throws IOException {
-          FileWriter file = makeFile("links");
-
-          Document scrape = Scraper.getHTML("https://en.wikipedia.org/wiki/Prime_number");
-
-          List<String> links = Scraper.getLinks(scrape);
-
-          writeToFile(file,links);
     }
 }
