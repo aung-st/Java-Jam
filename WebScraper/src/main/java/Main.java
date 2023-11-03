bef1da0bdc09face3a1da61b558e7b6a7407d9c7
CouchPotato <staung1445@gmail.com>
2023-11-03 15:29:58 +0000

    Add: Main class as entry point

---
+++ WebScraper/src/main/java/Main.java
@@ -0,0 +1,21 @@
+import org.jsoup.nodes.Document;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // fetch page
+        Document scrape = Scraper.getHTML("https://en.wikipedia.org/wiki/Prime_number");
+
+        // create file to store links
+        FileWriter fileLinks = ProcessData.makeFile("links");
+        List<String> links = Scraper.getLinks(scrape);
+        ProcessData.writeToFile(fileLinks,links);
+
+        // create file to store headers
+        FileWriter fileHeaders = ProcessData.makeFile("headers");
+        List<String> headers = Scraper.getHeaders(scrape);
+        ProcessData.writeToFile(fileHeaders,headers);
+    }
+}
