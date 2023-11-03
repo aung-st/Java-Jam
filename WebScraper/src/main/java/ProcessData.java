bd1f6e27ff97c803a9c5dff5d065d28f89daf5c9
CouchPotato <staung1445@gmail.com>
2023-11-03 15:14:46 +0000

    Add: method to name and create files into a data directory

---
+++ WebScraper/src/main/java/ProcessData.java
@@ -0,0 +1,33 @@
+import org.jsoup.nodes.Document;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ProcessData {
+    public static FileWriter makeFile(String name) throws IOException {
+        File file = new File(name + ".csv");
+        try {
+            return new FileWriter(file);
+        }catch (IOException e){
+            System.out.println("Something went wrong...");
+        }
+        return new FileWriter(file);
+    }
+
+    public static void main(String[] args) throws IOException {
+          FileWriter file = makeFile("data/links");
+//        Document scrape = Scraper.getHTML("https://en.wikipedia.org/wiki/Prime_number");
+//        List<String> headings = Scraper.getHeaders(scrape);
+//        for (String s : headings) {
+//            System.out.println(s);
+//        }
+//
+//        List<String> links = Scraper.getLinks(scrape);
+//        for (String l: links) {
+//            System.out.println(l);
+//        }
+    }
+}
