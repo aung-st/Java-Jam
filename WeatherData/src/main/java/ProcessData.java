b20727f514e410243704aca7b75d938c26e7aad8
CouchPotato <staung1445@gmail.com>
2023-11-04 18:05:49 +0000

    Amend: write to file as a json instead of csv

--- WeatherData/src/main/java/ProcessData.java
+++ WeatherData/src/main/java/ProcessData.java
@@ -1,3 +1,4 @@
+import com.google.gson.Gson;
 import org.json.JSONObject;
 import java.io.File;
 import java.io.FileWriter;
@@ -8,7 +9,7 @@ import java.util.Set;
 public class ProcessData {
     public static FileWriter makeFile(String name) throws IOException {
         // create a file in the data directory in WeatherData as a csv
-        File file = new File("data/" + name + ".csv");
+        File file = new File("data/" + name + ".json");
         try {
             return new FileWriter(file);
         } catch (IOException e) {
@@ -25,26 +26,9 @@ public class ProcessData {
         }
     }
 
-    public static void writeHeaders(FileWriter file) throws IOException {
-        String[] headers = {"NO2","O3","SO2","PM2.5","PM10","CO","overall_aqi"};
-        System.out.println(Arrays.toString(headers));
-        file.write(Arrays.toString(headers).replaceAll("]","").replaceAll("\\[",""));
+    public static void writeToFile(FileWriter file, JSONObject raw_json) throws IOException{
+        Gson gson = new Gson();
+        gson.toJson(raw_json, file);
         file.close();
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException {
-        JSONObject response = APICall.call();
-        System.out.println(response.get("NO2"));
-        System.out.println(response.get("O3"));
-        System.out.println(response.get("SO2"));
-        System.out.println(response.get("PM2.5"));
-        System.out.println(response.get("PM10"));
-        System.out.println(response.get("CO"));
-        System.out.println(response.get("overall_aqi"));
-
-        FileWriter airQualityData = ProcessData.makeFile("airQualityData");
-        writeHeaders(airQualityData);
-
-
     }
 }
